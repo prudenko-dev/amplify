fffff7916d639998cf10e0b19c53b7da37bd7a02
pasharudenko <pasharudenko210@gmail.com>
2025-04-14 23:24:16 +0300

    update

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -8,7 +8,8 @@ const schema = a.schema({
       name: a.string(),
     })
     .returns(a.string())
-    .handler(a.handler.function(sayHello)),
+    .handler(a.handler.function(sayHello))
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
